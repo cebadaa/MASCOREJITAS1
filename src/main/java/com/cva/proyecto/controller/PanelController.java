b1f33e4a35b676e07d62b68029fcf90dd280c56e
Christian Apaza <christian.apaza.pizarro@gmail.com>
2023-05-13 09:23:54 -0500

    se conectan todas las paginas con sus urls

--- src/main/java/com/cva/proyecto/controller/PanelController.java
+++ src/main/java/com/cva/proyecto/controller/PanelController.java
@@ -19,6 +19,23 @@ public class PanelController {
     public String login(){
         return "login";
     }
+    @RequestMapping("/nosotros")
+    public String nosotros(){
+        return "about";
+    }
+    @RequestMapping("/shop")
+    public String shop(){
+        return "shop";
+    }
+    @RequestMapping("/furniture")
+    public String furniture(){
+        return "furniture";
+    }
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    
 
 
 }
