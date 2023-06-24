55d5b488c05185bc50edc4551672f766eaa56183
Christian Apaza <christian.apaza.pizarro@gmail.com>
2023-06-24 15:11:46 -0500

    correccion de errores service y controller

--- src/main/java/com/cva/proyecto/controller/ComprasController.java
+++ src/main/java/com/cva/proyecto/controller/ComprasController.java
@@ -12,26 +12,5 @@ import com.cva.proyecto.model.entidad.Producto;
 import com.cva.proyecto.model.service.ProductoService;
 
 public class ComprasController {
-    @Autowired
-    private ProductoService productoService;
-
-    @Autowired
-    private PedidoService pedidoService;
-
-    @GetMapping("/compras")
-    public String mostrarPaginaCompras(Model model){
-        List<Producto> productos = productoService.mostrarProductos();
-        model.addAttribute("productos", productos);
-        return "/compras";
-    }
-
-    @PostMapping("/compras/agregar")
-    public String agregarCarrito(@RequestParam("id_pro") Long id_pro, @RequestParam("cantidad") int cantidad){
-       Producto producto = productoService.obtenerProductoId(id_pro);
-       for(CarritoItem item : carrito){
-        if
-       }
-    }
-
-    }
+    
 }
