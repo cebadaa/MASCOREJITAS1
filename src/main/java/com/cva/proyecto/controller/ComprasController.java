ee57b03d596dce236c881db8ea7270a859cb98bb
cva <cebada.v1@gmail.com>
2023-06-24 14:37:48 -0500

    errores xd

--- src/main/java/com/cva/proyecto/controller/ComprasController.java
+++ src/main/java/com/cva/proyecto/controller/ComprasController.java
@@ -5,33 +5,60 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cva.proyecto.model.dao.IComprasDAO;
 import com.cva.proyecto.model.entidad.Producto;
+import com.cva.proyecto.model.service.ICarroService;
 import com.cva.proyecto.model.service.ProductoService;
 
 public class ComprasController {
     @Autowired
-    private ProductoService productoService;
+    private ICarroService carroService;
 
     @Autowired
-    private PedidoService pedidoService;
+    private IComprasDAO comprasDAO;
 
-    @GetMapping("/compras")
-    public String mostrarPaginaCompras(Model model){
-        List<Producto> productos = productoService.mostrarProductos();
-        model.addAttribute("productos", productos);
-        return "/compras";
+    @GetMapping("/agregar-al-carrito/{idProducto}")
+    public String agregarAlCarrito(@PathVariable("idProducto") Long idProducto,
+                                   @RequestParam("cantidad") int cantidad) {
+        // Aquí puedes obtener el producto de la base de datos según el id
+        Producto producto = comprasDAO.obtenerProductoPorId(idProducto);
+
+        // Agregar el producto al carrito
+        carroService.agregarProducto(producto, cantidad);
+
+        return "redirect:/compras";
     }
 
-    @PostMapping("/compras/agregar")
-    public String agregarCarrito(@RequestParam("id_pro") Long id_pro, @RequestParam("cantidad") int cantidad){
-       Producto producto = productoService.obtenerProductoId(id_pro);
-       for(CarritoItem item : carrito){
-        if
-       }
+    @GetMapping("/carrito")
+    public String verCarrito(Model model) {
+        List<CarritoItem> carrito = carroService.obtenerCarrito();
+        double total = carroService.calcularTotalCarrito();
+
+        model.addAttribute("carrito", carrito);
+        model.addAttribute("total", total);
+
+        return "carrito";
     }
 
+    @PostMapping("/realizar-compra")
+    public String realizarCompra(@ModelAttribute("pedidoForm") PedidoForm pedidoForm) {
+        // Crear un objeto Pedido a partir de los datos del formulario
+        ComprasEntity pedido = new ComprasEntity();
+        pedido.setFechaPedido(new Date());
+        pedido.setCliente(pedidoForm.getCliente());
+        pedido.setProductos(carroService.obtenerCarrito());
+
+        // Guardar el pedido en la base de datos
+        comprasDAO.guardarPedido(pedido);
+
+        // Vaciar el carrito
+        carroService.vaciarCarro();
+
+        return "redirect:/dashboard/pedidos";
     }
 }
